ef7cca605c2083cf291d15acab95eb6d344a4c4b
chaeeun <chaeeun218@gmail.com>
2017-05-28 00:45:23 +0900

    봉사진행 상세 페이지에 카드뷰페이저 추가

---
+++ app/src/main/java/com/midas/donation_android_app/volunteer/CardAdapter.java
@@ -0,0 +1,15 @@
+package com.midas.donation_android_app.volunteer;
+
+
+import android.support.v7.widget.CardView;
+
+public interface CardAdapter {
+
+    int MAX_ELEVATION_FACTOR = 8;
+
+    float getBaseElevation();
+
+    CardView getCardViewAt(int position);
+
+    int getCount();
+}
