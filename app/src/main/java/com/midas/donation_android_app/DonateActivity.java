683805658995d1a094a56a9f0396c43a661dab11
sr9305@gmail.com <sr9305@gmail.com>
2017-05-27 17:20:59 +0900

    donate 내용 수정

--- app/src/main/java/com/midas/donation_android_app/DonateActivity.java
+++ app/src/main/java/com/midas/donation_android_app/DonateActivity.java
@@ -1,7 +1,10 @@
 package com.midas.donation_android_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TabHost;
 
@@ -41,25 +44,45 @@ public class DonateActivity extends AppCompatActivity {
 
         //tab1 ListView
         tab1list = (ListView)findViewById(R.id.tab1);
-        ArrayList<Donation> donateArr = new ArrayList<Donation>();
-        Donation d1 = new Donation("제목1", "2017.05.27", 1000, 10000);
-        Donation d2 = new Donation("제목2", "2017.05.27", 1000, 10000);
-        donateArr.add(d1);
-        donateArr.add(d2);
+        ArrayList<Donation> donateArr1 = new ArrayList<Donation>();
+        Donation d1 = new Donation("제목1", "2017.05.27", 1000, 10000, 1);
+        Donation d2 = new Donation("제목2", "2017.05.27", 1000, 10000, 1);
+        donateArr1.add(d1);
+        donateArr1.add(d2);
 
-        DonateAdapter da = new DonateAdapter(this, R.layout.donate_row, donateArr);
-        tab1list.setAdapter(da);
+        DonateAdapter da1 = new DonateAdapter(this, R.layout.donate_row, donateArr1);
+        tab1list.setAdapter(da1);
 
         //tab2 ListView
         tab2list = (ListView)findViewById(R.id.tab2);
-
-        //DonateAdapter da = new DonateAdapter(this, R.layout.donate_row, donateArr);
-        tab1list.setAdapter(da);
+        ArrayList<Donation> donateArr2 = new ArrayList<Donation>();
+        Donation d3 = new Donation("제목1", "2017.05.27", 1000, 10000, 2);
+        Donation d4 = new Donation("제목2", "2017.05.27", 1000, 10000, 2);
+        donateArr2.add(d1);
+        donateArr2.add(d2);
+        DonateAdapter da2 = new DonateAdapter(this, R.layout.donate_row, donateArr2);
+        tab2list.setAdapter(da2);
 
         //tab3 ListView
         tab3list = (ListView)findViewById(R.id.tab3);
-
         //DonateAdapter da = new DonateAdapter(this, R.layout.donate_row, donateArr);
-        tab3list.setAdapter(da);
+        ArrayList<Donation> donateArr3 = new ArrayList<Donation>();
+        Donation d5 = new Donation("제목1", "2017.05.27", 1000, 10000, 3);
+        Donation d6 = new Donation("제목2", "2017.05.27", 1000, 10000, 3);
+        donateArr2.add(d1);
+        donateArr2.add(d2);
+        DonateAdapter da3 = new DonateAdapter(this, R.layout.donate_row, donateArr3);
+        tab3list.setAdapter(da3);
+
+
+
+
+        //아이템 클릭하면 다음 화면으로 넘어가기
+        tab1list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
