9067f63a0b922950d7ad74b8f2c7a36f62bbe4a5
chaeeun <chaeeun218@gmail.com>
2017-05-27 22:29:28 +0900

    봉사 진행 설명 페이지에 X버튼, 참여하기 버튼 추가

---
+++ app/src/main/java/com/midas/donation_android_app/volunteer/VtDetailActivity.java
@@ -0,0 +1,28 @@
+package com.midas.donation_android_app.volunteer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.midas.donation_android_app.R;
+
+public class VtDetailActivity extends AppCompatActivity {
+
+    ImageButton backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vt_detail);
+        backButton = (ImageButton)findViewById(R.id.back_btn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/midas/donation_android_app/volunteer/VtListFragment.java
+++ app/src/main/java/com/midas/donation_android_app/volunteer/VtListFragment.java
@@ -2,6 +2,7 @@ package com.midas.donation_android_app.volunteer;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -100,9 +101,9 @@ public class VtListFragment extends Fragment {
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    Intent intent = new Intent(getActivity(), VolunteerDetailActivity.class);
-//                    startActivity(intent);
-                    Toast.makeText(getContext(), "클릭", Toast.LENGTH_SHORT);
+                    Intent intent = new Intent(getActivity(), VtDetailActivity.class);
+                    startActivity(intent);
+
 
                 }
             });
