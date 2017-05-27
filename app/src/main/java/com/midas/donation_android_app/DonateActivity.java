3bf6f35e354153d05043e1fbe662442fa713d740
sr9305@gmail.com <sr9305@gmail.com>
2017-05-27 21:53:33 +0900

    DB 관련 retrofit gradle 추가 및 DB 검색 추가

--- app/src/main/java/com/midas/donation_android_app/DonateActivity.java
+++ app/src/main/java/com/midas/donation_android_app/DonateActivity.java
@@ -1,6 +1,7 @@
 package com.midas.donation_android_app;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,7 +9,15 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TabHost;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 public class DonateActivity extends AppCompatActivity {
     TabHost tabHost;
@@ -87,4 +96,58 @@ public class DonateActivity extends AppCompatActivity {
             }
         });
     }
+    //value object 작성 - value object가 뭔지 모르겠음 ㅠㅠ
+    public class test {
+        private String ret;
+
+        public String getRet() {
+            return ret;
+        }
+
+        public void setRet(String ret) {
+            this.ret = ret;
+        }
+    }
+
+
+    public class Danate1 extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+            DonateService service = new repo().getDonateService();
+            Call<test> c = service.DonateInfo(params[0]);
+            try {
+                c.execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
+
+
+
+    //siteID 목록 만들기 service
+    public interface DonateService {
+        @FormUrlEncoded
+        @POST("donate1.php") // '/'를 붙이지 않음
+        //siteurl으로 찾는 예시
+        public Call<test> DonateInfo(@Field("username") String username);
+    }
+    //서비스 생성 및 반환
+    public class repo {
+        public DonateService getDonateService(){
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("192.168.0.48/") // '/'를 꼭 붙여주어야함!
+                    //json과 xml을 사용하기 위함
+                    .addConverterFactory(GsonConverterFactory.create())
+                //    .addConverterFactory(SimpleXmlConverterFactory.create()) //xml 사용하기 위해서
+                    .build();
+            DonateService service =  retrofit.create(DonateService.class);
+            return service;
+        }
+    }
+
 }
